1a5863e0c6e9ce7bae71d6a70f64ec8d65ae41c9
furby™ <devs@wabi.foundation>
2024-10-18 21:15:27 -0600

    Export otio namespace into swift as 'otio'.
    Signed-off-by: furby™ <devs@wabi.foundation>

--- Sources/interop/Export.swift
+++ Sources/interop/Export.swift
@@ -1,7 +1,9 @@
 //
-//  InteropEmpty.swift
+//  Export.swift
 //
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Contributors to the OpenTimelineIO project
 
 @_exported import OpenTime_CXX
+
+public typealias otio = opentime.v1_0
